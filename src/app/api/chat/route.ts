a79ddf394786496de3d56eabfa3e2e32b2cc7790
Supritam Mohanty <mohantysupritam@gmail.com>
2026-04-23 12:04:13 +0530

    Initial project setup

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,88 @@
+import connectDb from "@/lib/db";
+import Settings from "@/model/settings.model";
+import { GoogleGenAI } from "@google/genai";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+    try {
+        const { message, ownerId } = await req.json()
+        if (!message || !ownerId) {
+            return NextResponse.json(
+                { message: "message and owner id is required" },
+                { status: 400 }
+            )
+        }
+    await connectDb()
+        const setting = await Settings.findOne({ ownerId })
+        if (!setting) {
+            return NextResponse.json(
+                { message: "chat bot is not configured yet." },
+                { status: 400 }
+            )
+        }
+
+        const KNOWLEDGE=`
+        business name- ${setting.businessName || "not provided"}
+        supportEmail- ${setting.supportEmail || "not provided"}
+        knowledge- ${setting.knowledge ||" not provided"}
+        `
+     
+
+       const prompt = `
+You are a professional customer support assistant for this business.
+
+Use ONLY the information provided below to answer the customer's question.
+You may rephrase, summarize, or interpret the information if needed.
+Do NOT invent new policies, prices, or promises.
+
+
+
+--------------------
+BUSINESS INFORMATION
+--------------------
+${KNOWLEDGE}
+
+--------------------
+CUSTOMER QUESTION
+--------------------
+${message}
+
+--------------------
+ANSWER
+--------------------
+`;
+
+const ai = new GoogleGenAI({apiKey:process.env.GEMINI_API_KEY});
+ const res = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: prompt,
+  });
+
+const response= NextResponse.json(res.text)
+ response.headers.set("Access-Control-Allow-Origin", "*");
+    response.headers.set("Access-Control-Allow-Methods", "POST, OPTIONS");
+    response.headers.set("Access-Control-Allow-Headers", "Content-Type");
+return response
+
+    } catch (error) {
+ const response= NextResponse.json(
+                { message:`chat error ${error}` },
+                { status: 500 }
+            )
+  response.headers.set("Access-Control-Allow-Origin", "*");
+    response.headers.set("Access-Control-Allow-Methods", "POST, OPTIONS");
+    response.headers.set("Access-Control-Allow-Headers", "Content-Type");
+    return response
+    }
+}
+
+export const OPTIONS=async ()=>{
+   return NextResponse.json(null,{
+status:201,
+headers:{
+     "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "POST, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type",
+}
+   })
+}
