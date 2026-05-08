afe1ec74274b085bdfc48359cff6d432c4a5bbf4
Supritam Mohanty <mohantysupritam@gmail.com>
2026-05-08 22:20:48 +0530

    Initial project setup

--- src/app/api/auth/login/route.ts
+++
@@ -1,9 +0,0 @@
-import { scalekit } from "@/lib/scalekit";
-import { NextRequest, NextResponse } from "next/server";
-
-export async function GET(req:NextRequest) {
-    const redirectUri=`${process.env.NEXT_PUBLIC_APP_URL}/api/auth/callback`
-    const url=scalekit.getAuthorizationUrl(redirectUri)
-    console.log(url)
-    return NextResponse.redirect(url)
-}
