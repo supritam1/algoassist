60cd15ddcc4662d76143b21f84f9312acc0f1f59
S-M-Horo <simran.horo1999@gmail.com>
2026-05-09 12:19:35 +0530

    Updated Embed

---
+++ src/app/embed/page.tsx
@@ -0,0 +1,15 @@
+import EmbedClient from '@/components/EmbedClient'
+import { getSession } from '@/lib/getSession'
+import React from 'react'
+
+async function page() {
+    const session=await getSession()
+
+  return (
+    <>
+      <EmbedClient ownerId={session?.user?.id!}/>
+    </>
+  )
+}
+
+export default page
